02369583889de0d9bc816fb48472a9676237ae86
sithcode-key <sith.norvang@keyprod.com>
2023-06-23 18:53:31 +0200

    Ticket and Review model

--- litreview/reviews/views.py
+++ litreview/reviews/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from . import models
 
 
 @login_required
 def feed(request):
+    tickets = models.Ticket.objects.all()
     return render(request, 'home.html')
+
+
+@login_required
+def create_ticket(request):
+    # manage form
+    pass
