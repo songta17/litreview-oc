75c70c5bc942d6e8a20e30962af38aaf566a1202
sithcode-key <sith.norvang@keyprod.com>
2023-06-30 13:59:15 +0200

    userfollow model added

--- litreview/reviews/models.py
+++ litreview/reviews/models.py
@@ -36,3 +36,19 @@ class Review(models.Model):
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     time_created = models.DateTimeField(auto_now_add=True)
+
+
+class UserFollows(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+    followed_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='followed_by'
+    )
+
+    class Meta:
+        unique_together = ('user', 'followed_user')
